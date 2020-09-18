ddd3cfa6349bb201530c0c752299b0583cc72293
Jesse O'Donoghue <jesseodonoghue@gmail.com>
2020-09-18 14:07:00 -0400

    Update role functions

--- employee-manager.js
+++ employee-manager.js
@@ -1,6 +1,12 @@
 let mysql = require("mysql");
 let inquirer = require("inquirer");
 let cTable = require("console.table");
+const observe = require("inquirer/lib/utils/events");
+
+// Global variables
+let deptList = [];
+let roleList = [];
+let empList = [];
 
 // Define connection to local database
 let connection = mysql.createConnection({
@@ -87,10 +93,9 @@ function deptSearch() {
 }
 
 function listDept() {
-    let queryString = "SELECT name FROM department";
+    let queryString = "SELECT * FROM department";
 
     return new Promise((resolve, reject) => {
-
         connection.query(queryString, function(err, res) {
             if (err) {
                 reject(err);
@@ -98,12 +103,7 @@ function listDept() {
                 resolve(res);
             }
         });
-
-    })
-    .then(val => {
-        return val;
-    });
-    
+    });    
 }
 
 function viewDept() {
@@ -146,7 +146,10 @@ function addDept() {
                 if (err) throw err;
                 
                 console.clear();
-                console.log("Successfully added department named " + answer.addDept);
+                console.log(`
+                Successfully added ${answer.addDept} department.
+
+                `);
 
                 mainSearch();
             });
@@ -155,39 +158,223 @@ function addDept() {
 
 function delDept () {
 
-    let deptList = listDept();
+    listDept().then(res => {
+        
+        let choicesList = [];
+        for (let i = 0; i < res.length; i++) {
+            let obj = {};
+            obj["id"] = res[i].id;
+            obj["name"] = res[i].name;
 
-    console.log(deptList);
+            deptList.push(obj);
+            choicesList.push(obj.name);
+        }
 
-    // inquirer
-    //     .prompt({
-    //         name: "delDept",
-    //         type: "input",
-    //         message: "What is the name of the department you'd like to delete?",
-    //         validate: (value) => {
-    //             if (value === "" || value === null) {
-    //                 return "You must enter a value!";
-    //             } else if (isNaN(value)) {
-    //                 return true;
-    //             } else {
-    //                 return "Department name can't be a number.";
-    //             }
-    //         }
-    //     })
-    //     .then(function(answer) {
-    //         let queryString = "DELETE FROM department WHERE name=?";
+        inquirer
+            .prompt({
+                name: "delDept",
+                type: "list",
+                message: "Which department would you like to delete?",
+                choices: choicesList
+            })
+            .then(function(answer) {
+                let queryString = "DELETE FROM department WHERE name=?";
+    
+                connection.query(queryString, answer.delDept, function(err, res) {
+                    if (err) throw err;
+    
+                    console.clear();
+                    console.log(`
+                    Successfully deleted ${answer.delDept} department.
+                    
+                    `);                
 
-    //         connection.query(queryString, answer.delDept, function(err, res) {
-    //             if (err) throw err;
+                    mainSearch();
+                });
+            });
+    });
+}
 
-    //             if (res.rowsAffected === 0) {
-    //                 console.clear();
-    //                 console.log("No department found named " + answer.delDept);
-    //             } else {
-    //                 console.clear();
-    //                 console.log("Successfully deleted department named " + answer.delDept);
-    //             }
-    //         })
-    //     })
+function deptBudget () {
 
+}
+
+
+// Role functions
+function roleSearch() {
+    inquirer
+        .prompt({
+            name: "role",
+            type: "list",
+            message: "Which role task would you like to perform?",
+            choices: [
+                "View all roles",
+                "Add a role",
+                "Delete a role"
+            ]
+        })
+        .then(function(answer) {
+            switch (answer.role) {
+            case "View all roles":
+                viewRole();
+                break;
+            
+            case "Add a role":
+                addRole();
+                break;
+
+            case "Delete a role":
+                delRole();
+                break;
+            }            
+        });
+}
+
+function listRole() {
+    let queryString = "SELECT * FROM role";
+
+    return new Promise((resolve, reject) => {
+        connection.query(queryString, function(err, res) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+        });
+    });    
+}
+
+function viewRole() {
+    
+    let queryString = "SELECT title FROM role";
+
+    connection.query(queryString, function (err, res) {
+
+        if (err) throw err;
+        
+        console.clear();
+        console.table(res);
+
+        mainSearch();
+    });
+}
+
+function addRole() {
+
+    listDept().then(res => {
+
+        let choicesList = [];
+
+        for (let i = 0; i < res.length; i++) {
+            let obj = {};
+            obj["id"] = res[i].id;
+            obj["name"] = res[i].name;
+
+            deptList.push(obj);
+            choicesList.push(obj.name);
+        }
+        
+        inquirer
+            .prompt([
+            {    
+                name: "title",
+                type: "input",
+                message: "What is the title of the role you'd like to add?",
+                validate: (value) => {
+                    if (value === "" || value === null) {
+                        return "You must enter a value!";
+                    } else if (isNaN(value)) {
+                        return true;
+                    } else {
+                        return "Role title can't be a number.";
+                    }
+                }
+            },
+            {
+                name: "salary",
+                type: "input",
+                message: "What is the salary of this role?",
+                validate: (value) => {
+                    if (value === "" || value === null) {
+                        return "You must enter a value!";
+                    } else if (isNaN(value)) {
+                        return "Salary must be a number.";
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            {
+                name: "department",
+                type: "list",
+                message: "Which department does this role belong to?",
+                choices: choicesList
+            }])
+            .then(function(answer) {
+                let roleId;
+
+                function func () {
+                    for (let i = 0; i < deptList.length; i++) {
+                        if (deptList[i].name === answer.department) {
+                            return deptList[i].id;                            
+                        }
+                    }
+                };
+
+                roleId = func();
+
+                let queryString = "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)";
+    
+                connection.query(queryString, [answer.title, answer.salary, roleId], function(err, res) {
+                    if (err) throw err;
+                    
+                    console.clear();
+                    console.log(`                    
+                    Successfully added ${answer.title} role.
+    
+                    `);
+    
+                    mainSearch();
+                });
+            });        
+    });
+}
+
+function delRole () {
+
+    listRole().then(res => {
+        
+        let choicesList = [];
+        for (let i = 0; i < res.length; i++) {
+            let obj = {};
+            obj["id"] = res[i].id;
+            obj["title"] = res[i].title;
+
+            roleList.push(obj);
+            choicesList.push(obj.title);
+        }
+        
+        inquirer
+            .prompt({
+                name: "delRole",
+                type: "list",
+                message: "Which role would you like to delete?",
+                choices: choicesList
+            })
+            .then(function(answer) {
+                let queryString = "DELETE FROM role WHERE title=?";
+    
+                connection.query(queryString, answer.delRole, function(err, res) {
+                    if (err) throw err;
+    
+                    console.clear();
+                    console.log(`
+                    Successfully deleted ${answer.delRole} department.
+
+                    `);          
+                    
+                    mainSearch();
+                });
+            });
+    });
 }

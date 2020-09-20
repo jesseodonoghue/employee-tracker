1febbc1f8699f1b82054eb5b13edce46c5d170d6
Jesse O'Donoghue <jesseodonoghue@gmail.com>
2020-09-20 13:16:08 -0400

    Work on employee functions

--- employee-manager.js
+++ employee-manager.js
@@ -7,6 +7,10 @@ const observe = require("inquirer/lib/utils/events");
 let deptList = [];
 let roleList = [];
 let empList = [];
+let choicesDeptList = [];
+let choicesRoleList = [];
+let choicesEmpList = [];
+
 
 // Define connection to local database
 let connection = mysql.createConnection({
@@ -108,7 +112,7 @@ function listDept() {
 
 function viewDept() {
     
-    let queryString = "SELECT name FROM department";
+    let queryString = "SELECT * FROM department";
 
     connection.query(queryString, function (err, res) {
 
@@ -160,14 +164,15 @@ function delDept () {
 
     listDept().then(res => {
         
-        let choicesList = [];
+        choicesDeptList = [];
+
         for (let i = 0; i < res.length; i++) {
             let obj = {};
             obj["id"] = res[i].id;
             obj["name"] = res[i].name;
 
             deptList.push(obj);
-            choicesList.push(obj.name);
+            choicesDeptList.push(obj.name);
         }
 
         inquirer
@@ -175,7 +180,7 @@ function delDept () {
                 name: "delDept",
                 type: "list",
                 message: "Which department would you like to delete?",
-                choices: choicesList
+                choices: choicesDeptList
             })
             .then(function(answer) {
                 let queryString = "DELETE FROM department WHERE name=?";
@@ -246,7 +251,7 @@ function listRole() {
 
 function viewRole() {
     
-    let queryString = "SELECT title FROM role";
+    let queryString = "SELECT * FROM role";
 
     connection.query(queryString, function (err, res) {
 
@@ -263,7 +268,7 @@ function addRole() {
 
     listDept().then(res => {
 
-        let choicesList = [];
+        choicesDeptList = [];
 
         for (let i = 0; i < res.length; i++) {
             let obj = {};
@@ -271,7 +276,7 @@ function addRole() {
             obj["name"] = res[i].name;
 
             deptList.push(obj);
-            choicesList.push(obj.name);
+            choicesDeptList.push(obj.name);
         }
         
         inquirer
@@ -308,7 +313,7 @@ function addRole() {
                 name: "department",
                 type: "list",
                 message: "Which department does this role belong to?",
-                choices: choicesList
+                choices: choicesDeptList
             }])
             .then(function(answer) {
                 let roleId;
@@ -337,6 +342,231 @@ function addRole() {
                     mainSearch();
                 });
             });        
+    });
+}
+
+function delRole () {
+
+    listRole().then(res => {
+
+        choicesRoleList = [];
+        
+        for (let i = 0; i < res.length; i++) {
+            let obj = {};
+            obj["id"] = res[i].id;
+            obj["title"] = res[i].title;
+
+            roleList.push(obj);
+            choicesRoleList.push(obj.title);
+        }
+        
+        inquirer
+            .prompt({
+                name: "delRole",
+                type: "list",
+                message: "Which role would you like to delete?",
+                choices: choicesRoleList
+            })
+            .then(function(answer) {
+                let queryString = "DELETE FROM role WHERE title=?";
+    
+                connection.query(queryString, answer.delRole, function(err, res) {
+                    if (err) throw err;
+    
+                    console.clear();
+                    console.log(`
+                    Successfully deleted ${answer.delRole} department.
+
+                    `);          
+                    
+                    mainSearch();
+                });
+            });
+    });
+}
+
+
+// Employee functions
+function empSearch() {
+    inquirer
+        .prompt({
+            name: "emp",
+            type: "list",
+            message: "Which employee task would you like to perform?",
+            choices: [
+                "View all employees",
+                "Add an employee",
+                "Delete an employee"
+            ]
+        })
+        .then(function(answer) {
+            switch (answer.emp) {
+            case "View all employees":
+                viewEmp();
+                break;
+            
+            case "Add an employee":
+                addEmp();
+                break;
+
+            case "Delete an employee":
+                delEmp();
+                break;
+            }            
+        });
+}
+
+function listEmp() {
+    let queryString = "SELECT * FROM employee";
+
+    return new Promise((resolve, reject) => {
+        connection.query(queryString, function(err, res) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+        });
+    });    
+}
+
+function viewEmp() {
+    
+    let queryString = "SELECT * FROM employee";
+
+    connection.query(queryString, function (err, res) {
+
+        if (err) throw err;
+        
+        console.clear();
+        console.table(res);
+
+        mainSearch();
+    });
+}
+
+function addEmp() {
+
+    listRole().then(roles => {
+
+        choicesRoleList = [];
+
+        for (let i = 0; i < roles.length; i++) {
+            let obj = {};
+            obj["id"] = roles[i].id;
+            obj["title"] = roles[i].title;
+            obj["salary"] = roles[i].salary;
+            obj["department_id"] = roles[i].department_id;
+
+            roleList.push(obj);
+            choicesRoleList.push(obj.title);
+        }
+        
+        listEmp().then(emps => {
+
+            choicesEmpList = [];
+
+            for (let i = 0; i < emps.length; i++) {
+                let obj = {};
+                obj["id"] = emps[i].id;
+                obj["first_name"] = emps[i].first_name;
+                obj["last_name"] = emps[i].last_name;
+                obj["role_id"] = emps[i].role_id;
+                obj["manager_id"] = emps[i].manager_id;
+    
+                empList.push(obj);
+                choicesEmpList.push(obj.first_name + " " + obj.last_name);
+            }
+
+
+            inquirer
+                .prompt([
+                {    
+                    name: "fname",
+                    type: "input",
+                    message: "What is the first name of the new employee?",
+                    validate: (value) => {
+                        if (value === "" || value === null) {
+                            return "You must enter a value!";
+                        } else if (isNaN(value)) {
+                            return true;
+                        } else {
+                            return "First name can't be a number.";
+                        }
+                    }
+                },
+                {
+                    name: "lname",
+                    type: "input",
+                    message: "What is the last name of the new employee?",
+                    validate: (value) => {
+                        if (value === "" || value === null) {
+                            return "You must enter a value!";
+                        } else if (isNaN(value)) {
+                            return true;
+                        } else {
+                            return "Last name can't be a number.";
+                        }
+                    }
+                },
+                {
+                    name: "role",
+                    type: "list",
+                    message: "What is the new employee's role?",
+                    choices: choicesRoleList
+                },
+                {
+                    name: "manager",
+                    type: "list",
+                    message: "Who is the new employee's manager?",
+                    choices: choicesEmpList
+                }
+            ])
+                .then(function(answer) {
+                    let roleId;
+                    let manId;
+    
+                    function getRoleId () {
+                        for (let i = 0; i < roleList.length; i++) {
+                            if (roleList[i].title === answer.role) {
+                                return roleList[i].id;                            
+                            }
+                        }
+                    };
+
+                    function getManId () {
+                        for (let i = 0; i < empList.length; i++) {
+                            const chosenManager = answer.manager.split(" ");
+
+                            if (empList[i].first_name === chosenManager[0] && empList[i].last_name === chosenManager[1]) {
+                                return empList[i].id;                            
+                            }
+                        }
+                    };
+    
+                    roleId = getRoleId();
+                    manId = getManId();
+
+                    console.log(`
+                    Role ID equals ${roleId}
+                    
+                    Manager ID equals ${manId}`);
+    
+                    // let queryString = "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)";
+        
+                    // connection.query(queryString, [answer.title, answer.salary, roleId], function(err, res) {
+                    //     if (err) throw err;
+                        
+                    //     console.clear();
+                    //     console.log(`                    
+                    //     Successfully added ${answer.title} role.
+        
+                    //     `);
+        
+                    //     mainSearch();
+                    // });
+                });   
+        });
     });
 }
 
